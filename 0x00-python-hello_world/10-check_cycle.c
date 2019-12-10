21c2418ae0979cddddb1257a9810ccf22feaa3d1
camilooob <camilobaq@hotmail.com>
2019-12-09 22:00:58 -0500

    ok betty

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,7 +5,7 @@
 /**
  * check_cycle - count the cycle
  * @list: input
- *
+ * Return: 0 if ok and 1 if not
  */
 
 int check_cycle(listint_t *list)
@@ -13,18 +13,18 @@ int check_cycle(listint_t *list)
 
 	listint_t *current;
 
-	if(list)
+	if (list)
 	{
 		while (list != NULL)
 		{
 			current = list;
 			list = list->next;
-			if(current <= list)
+			if (current <= list)
 				return (1);
 		}
-		return(0);
+		return (0);
 	}
-	return(0);
+	return (0);
 
 }
 
