3a2fd1c427c082d56b1efb640b29d49969eae922
camilooob <camilobaq@hotmail.com>
2019-12-09 21:35:42 -0500

    --- Auto-Git Commit ---

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - count the cycle
+ * @list: input
+ *
+ */
+int check_cycle(listint_t *list)
+{
+  return (0);
+}
+
