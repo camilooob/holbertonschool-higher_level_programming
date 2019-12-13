72db9396fb972483b7f2f268e74b0c66897401eb
camilooob <camilobaq@hotmail.com>
2019-12-12 19:08:27 -0500

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
         print("{} argument:".format(1))
         print("{}: {}".format(1, sys.argv[1]))
     else:
-        print("{} arguments:".format(len(sys.argv) -1 ))
+        print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
