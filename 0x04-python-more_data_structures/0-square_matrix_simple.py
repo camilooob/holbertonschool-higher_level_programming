54975939ce14861199884a7979173f9efb970cb4
camilooob <camilobaq@hotmail.com>
2019-12-19 20:53:08 -0500

    [fix] pep 8 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = []
+    a = []
     for i in matrix:
         squares = list(map(lambda x: x**2, i))
-        new.append(squares)
-    return new
+        a.append(squares)
+    return a
