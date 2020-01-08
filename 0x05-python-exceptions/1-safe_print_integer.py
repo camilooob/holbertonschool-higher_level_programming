12f9f368f720cd7f89792ae45eb57eb0054bde3f
camilooob <camilobaq@hotmail.com>
2020-01-08 16:49:37 -0500

    [fix] print format

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        int(value)
-        print(value)
-        return True
-
+        if int(value):
+            print("{}".format(value))
+            return True
     except ValueError:
         return False
