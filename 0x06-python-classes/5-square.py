dde95993154ab39e3d2ec31b4a7b217212ff16ad
camilooob <camilobaq@hotmail.com>
2020-01-09 20:25:42 -0500

    fix pep8

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
@@ -14,7 +13,6 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-            return
         for i in range(self.__size):
             print("#"*self.size)
 
