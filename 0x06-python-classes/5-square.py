671d1bfed0350a35623dbc0ffad2b5070145bbf8
camilooob <camilobaq@hotmail.com>
2020-01-09 20:03:02 -0500

    add return

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -14,6 +14,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
+            return
         for i in range(self.__size):
             print("#"*self.size)
 
