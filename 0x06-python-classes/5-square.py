2327834a370a0247adbb6604412a2277f6e86810
camilooob <camilobaq@hotmail.com>
2020-01-09 20:02:26 -0500

    [06] Fix pep8

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,30 @@
+
 #!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#"*self.size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, sizze):
+        if not isinstance(sizze, int):
+            raise TypeError("size must be an integer")
+        if sizze < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = sizze
