874cc83191ca4cc6d9757abf411e16b877513310
camilooob <camilobaq@hotmail.com>
2020-01-09 19:07:50 -0500

    [05 and 06]

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,3 +9,13 @@ class Square:
 
     def area(self):
         return (self.__size ** 2)
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, sizze):
+        if not isinstance(sizze, int):
+            raise TypeError("size must be an integer")
+        if sizze < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = sizze
