6ed40735207dbd100905a886f1f5c49bc887fa89
camilooob <camilobaq@hotmail.com>
2020-01-12 19:20:07 -0500

    02 ok

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""This is  the "matrix_divided" function.
+The matrix_divided function divided matrix. For example,
+>>> matrix_divided([[1, 2, 3],[4, 5, 6]], 3)
+[[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
+"""
+def matrix_divided(matrix, div):
+    matrix1 = []
+    matrix2 = []
+    matrix10 = [matrix1, matrix2]
+    z = 1
+    for i in matrix:
+        for j in i:
+            if z <= 3:
+                matrix1.append(float("{0:.2f}".format((j / div))))
+            else:
+                matrix2.append(float("{0:.2f}".format((j / div))))
+            z += 1
+    return matrix10
--- 0x07-python-test_driven_development/example.py
+++ 0x07-python-test_driven_development/example.py
@@ -1,83 +1,21 @@
-"""This is  the "example" module.
 
-The example module supplies one fuction, factorial(). For example,
-Test 0:
->>> factorial(5)
-120
-"""
+def matrix_divided(matrix, div):
+    matrix1 = []
+    matrix2 = []
+    matrix10 = [matrix1, matrix2]
+    z = 1
+    for i in matrix:
+        for j in i:
+            if z <= 3:
+                matrix1.append(float("{0:.2f}".format((j / div))))
+            else:
+                matrix2.append(float("{0:.2f}".format((j / div))))
+            z += 1
+    return matrix10
 
-def factorial(n):
-    """Return the factorial of n, an exact integer >= 0.
+def main():
+    matrix = [[1, 2, 3],[4, 5, 6]]
+    print(matrix_divided(matrix, 3))
+    print(matrix)
 
-    Test 1:
-    >>> [factorial(n) for n in range(6)]
-    [1, 1, 2, 6, 24, 120]
-
-    Test 2:
-    >>> factorial(30)
-    265252859812191058636308480000000
-
-    Test 3:
-    >>> factorial(-1)
-    Traceback (most recent call last):
-        ...
-    ValueError: n must be >= 0
-
-    Test 4 - Factorials of floats are OK, but the float must be an exact integer:
-    >>> factorial(30.1)
-    Traceback (most recent call last):
-        ...
-    ValueError: n must be exact integer
-
-    Test 5:
-    >>> factorial(30.0)
-    265252859812191058636308480000000
-
-    Test 6 - It must also not be ridiculously large:
-    >>> factorial(1e100)
-    Traceback (most recent call last):
-       ...
-    OverflowError: n too large
-    """
-    import math
-    if not n >= 0:
-        raise ValueError("n must be >= 0")
-    if math.floor(n) != n:
-        raise ValueError("n must be exact integer")
-    if n+1 == n: #catch a vale like 1e300
-        raise OverflowError("n too large")
-    result = 1
-    factor = 2
-    while factor <= n:
-        result *= factor
-        factor += 1
-    return result
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-    """
-    Test 1:
-    >>> add_integer(0, 0)
-    0
-
-    Test 2:
-    >>> add_integer(0, -3)
-    -3
-
-    Test 3:
-    >>> add_integer(-1, b)
-    Traceback (most recent call last):
-        ...
-    TypeError: b must be an integer
-
-    Test 4:
-    >>> add_integer(a, 5)
-    Traceback (most recent call last):
-        ...
-    TypeError: a must be an integer
-
-    Test 5:
-    >>> add_integer(10.27564.10.5647335)
-    20
-    """
+main()
