d5f55643db65cc93a708beff5bebdf28a2f8c247
camilooob <camilobaq@hotmail.com>
2020-01-16 13:55:52 -0500

    C

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "list.h"
+/**
+* PalindromeRecur - Check if linked list is palindrome.
+* @left: Head of list
+* @right: Last element take it in recursion.
+*/
+int PalindromeRecur(listint_t **left, listint_t *right)
+{
+      int pal;
+
+      if (right == NULL)
+          return (1);
+
+      pal = PalindromeRecur(left, right->next);
+      if (pall = 0)
+          return (0)
+}
+
+/**
+* is_palindrome - Check if linked list is palindrome.
+* @head: give list
+* @right: Last element take it in recursion.
+*/
+int is_palindrome(listint_t **head):
