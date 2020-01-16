1352e1cfe0cdc46ca5cdfa8ac07bc448911bed6b
camilooob <camilobaq@hotmail.com>
2020-01-16 15:43:02 -0500

    Fix check

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,7 +47,8 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     @position.setter
     def position(self, value):
