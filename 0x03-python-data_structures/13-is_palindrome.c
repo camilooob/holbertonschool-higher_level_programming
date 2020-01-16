e76756e9fb30f76fa999c2a135785b17a92132c0
camilooob <camilobaq@hotmail.com>
2020-01-16 14:12:23 -0500

    c 0x03 palindrome Angel

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,24 +1,37 @@
 #include "list.h"
 /**
-* PalindromeRecur - Check if linked list is palindrome.
-* @left: Head of list
-* @right: Last element take it in recursion.
-*/
+ * PalindromeRecur - Check if linked list is palindrome.
+ * @left: Head of list
+ * @right: Last element take it in recursion.
+ */
 int PalindromeRecur(listint_t **left, listint_t *right)
 {
-      int pal;
+	int pal;
 
-      if (right == NULL)
-          return (1);
+	if (right == NULL)
+		return (1);
 
-      pal = PalindromeRecur(left, right->next);
-      if (pall = 0)
-          return (0)
+	pal = PalindromeRecur(left, right->next);
+	if (pal = 0)
+		return (0);
+
+	pal = (righth->n == (*left)->n);
+
+	*left = (*left)->next;
+	return (pal);
 }
 
 /**
-* is_palindrome - Check if linked list is palindrome.
-* @head: give list
-* @right: Last element take it in recursion.
-*/
+ * is_palindrome - Check if linked list is palindrome.
+ * @head: give list
+ * @right: Last element take it in recursion.
+ */
 int is_palindrome(listint_t **head):
+{
+	int res;
+
+	if (!head)
+		return (0);
+	res = RecursivePalindrome(head, *head)
+		return (res);
+}
