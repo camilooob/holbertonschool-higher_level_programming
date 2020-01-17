442e6333f027c270fbbadc55b3e641e1e798eefb
camilooob <camilobaq@hotmail.com>
2020-01-17 18:15:39 -0500

    Fix 01 Test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""This is  the "add_integer" module.
-The add_interger module add a with b. For example,
->>> add_integer(1, 4)
-5
-"""
-
-
 def add_integer(a, b=98):
-    """This function sum a and b.
+    """
+    This function sum a and b.
     Return the result int datatype.
-    Raise TypeError if data is diferrent that int."""
+    Raise TypeError if data is diferrent that int.
+    """
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
+
     if not (isinstance(b, int) or isinstance(b, float)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
+
+    if isinstance(a, float) or isinstance(a, int):
         a = int(a)
-    if isinstance(b, float):
+
+    if isinstance(b, float) or isinstance(b, int):
         b = int(b)
+
     if isinstance(a, int) and isinstance(b, int):
         return int(a + b)
