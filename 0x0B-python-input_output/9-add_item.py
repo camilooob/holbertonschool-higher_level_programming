baaff5cb1309c155a2df9eeb6e47e042e56dd238
camilooob <camilobaq@hotmail.com>
2020-01-23 18:29:35 -0500

    fix pep8 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-
-from sys import argv
-from os import path
+import sys
+import json
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+if __name__ == "__main__":
+    try:
+        json_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        json_list = []
 
-if path.isfile(filename):
-    mylist = load_from_json_file(filename)
-else:
-    mylist = []
-
-mylist += sys.argv[1:]
-save_to_json_file(my_list, filename)
+    for i in range(1, len(sys.argv)):
+        json_list.append(sys.argv[i])
+    save_to_json_file(json_list, "add_item.json")
