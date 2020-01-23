fa874a9d8e8b284172db852f198dcb9c7a2eada6
camilooob <camilobaq@hotmail.com>
2020-01-23 18:20:36 -0500

    100 my int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+class
+My int
+"""
+class MyInt(int):
+    """ Change eq and ne"""
+    def __eq__(self, num):
+        """ Change == to !="""
+        return int(self) != int(num)
+
+    def __ne__(self, num):
+        """ Change != to =="""
+        return int(self) == int(num)
