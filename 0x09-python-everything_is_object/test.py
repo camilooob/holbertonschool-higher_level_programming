ae122197608f23c9c2962b66bfdbccfda3e09553
camilooob <camilobaq@hotmail.com>
2020-01-23 13:31:53 -0500

    Piramid

---
+++ 0x09-python-everything_is_object/test.py
@@ -0,0 +1,15 @@
+
+# Python program to demonstrate the
+# use of join function to join list
+# elements with a character.
+
+list1 = ['1', '2', '3', '4']
+
+s = "^"
+# joins elements of list1 by '-'
+# and stores in sting s
+s = s.join(list1)
+
+# join use to join a list of
+# strings to a separator s
+print(s)
