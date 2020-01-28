f9b1f73897de19bfc42a7a93dd84d2344cef4bf3
camilooob <camilobaq@hotmail.com>
2020-01-28 15:48:54 -0500

    00

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
+
+
 import unittest
 import pep8
 import inspect
-from models import rectangle, square, base
 
 
 class TestCodeFormat(unittest.TestCase):
@@ -9,6 +11,8 @@ class TestCodeFormat(unittest.TestCase):
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        mode_base = pep8style.check_files(['models/base.py'], ['models/rectangle.py'])
-        self.assertEqual(result.total_errors, 0,
+        mode_base = pep8style.check_files(['models/base.py',
+                                           'models/rectangle.py',
+                                           ])
+        self.assertEqual(mode_base.total_errors, 0,
                          "Found code style errors (and warnings).")
